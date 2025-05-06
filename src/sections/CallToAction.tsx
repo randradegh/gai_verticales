40ba22243f5879a2df6051a568da1324d18848e0
Roberto Andrade Fonseca <randradedev@gmail.com>
2025-05-05 22:13:09 -0600

    Inserción de los detalles de inscripción.

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -37,12 +37,10 @@ const CallToAction = () => {
                 </p>
               </div>
               <div className="flex flex-col gap-2">
-                <a
-                  href="#"
-                  className="btn-primary w-full text-center"
-                >
-                  Inscríbete Ahora
-                </a>
+                <p className="text-secondary-700 text-base font-semibold mb-2">
+                  Para inscribirte, manda un correo a <a href="mailto:randradedev@gmail.com" className="text-primary-600 underline">randradedev@gmail.com</a> solicitando la inscripción al curso "Curso IA Generativa MX".<br/>
+                  A vuelta de correo se te solicitarán algunos datos académicos y se te detallará la forma de pago del curso.
+                </p>
                 <p className="text-sm text-secondary-500">
                   Plazas limitadas
                 </p>
